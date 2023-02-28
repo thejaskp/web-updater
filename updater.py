89cc411d4c5fcd40533fcf547d52e3af02378a88
thejaskp <thejaskuttipoyil@gmail.com>
2023-03-01 02:19:04 +0530

    updated with flask

--- updater.py
+++ updater.py
@@ -1,33 +1,48 @@
 import time
 import hashlib
 from urllib.request import urlopen, Request
+from flask import Flask, render_template
 
-link = 'https://members.linkedin.com/en-in/student/linkedin-premium'
+app = Flask(__name__)
 
-url = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
-response = urlopen(url).read()
+@app.route("/")
+@app.route("/home")
 
-currentHash = hashlib.sha224(response).hexdigest()
-print('running...')
-time.sleep(10)
-
-while True:
-    try:
-        response = urlopen(url).read()
-        currentHash = hashlib.sha224(response).hexdigest()
-        time.sleep(20)
-
-        response = urlopen(url).read()
-        newHash = hashlib.sha224(response).hexdigest()
-
-        if newHash == currentHash:
-            continue
-        else:
-            print('something changed')
+def checker():
+    url = Request('https://members.linkedin.com/en-in/student/linkedin-premium', headers={'User-Agent': 'Mozilla/5.0'})
+    response = urlopen(url).read()
+    currentHash = hashlib.sha224(response).hexdigest()
+    print('running...')
+    time.sleep(10)
+    while True:
+        try:
             response = urlopen(url).read()
             currentHash = hashlib.sha224(response).hexdigest()
+            time.sleep(20)
 
-    except Exception as e:
-        print("e")
+            response = urlopen(url).read()
+            newHash = hashlib.sha224(response).hexdigest()
+
+            if newHash == currentHash:
+                continue
+            else:
+                print('something changed')
+                response = urlopen(url).read()
+                currentHash = hashlib.sha224(response).hexdigest()
+
+        except Exception as e:
+            print("e")
+        return render_template("index.html")
+        
+
+if __name__ == '__main__':
+    app.run(debug = True, port=5001)
+
+
+
+
+
+
+
 
 
